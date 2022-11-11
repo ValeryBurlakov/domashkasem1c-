7407635288df31a187f470ae90f745be31b5cfc8
Valery <wogl@mail.ru>
2022-11-11 23:39:29 +0300

    zadacha2 complete

---
+++ 2zadacha/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая на вход принимает 2 числа и выдаёт, какое число больше, а какое меньше
+// a = 5, b = 10 -> max = 10
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine($"Максимальное число {number1}");
+    Console.WriteLine($"А минимальное число {number2}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {number2}");
+    Console.WriteLine($"А минимальное число {number1}");
+}
+// $"Квадрат числа {number} равен {square}"
